8f678a4638b7b601140b91d95c626982ae84ce34
Vladyslav Pekker <agilesteel@gmail.com>
2019-04-28 13:35:33 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# install-sublime-text
+# Installs [Sublime Text](https://www.sublimetext.com/) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://raw.githubusercontent.com/DevInsideYou/install-sublime-text/master/install-sublime-text.sh
+sudo chmod +x install-sublime-text.sh && ./install-sublime-text.sh
+```
---
+++ install-sublime-text.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# add apt key
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+# make sure https is enabled
+sudo apt install -y apt-transport-https
+
+# add repository
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# update cache
+sudo apt update
+
+# install sublime-text
+sudo apt install -y sublime-text
+
+# remove yourself
+rm $0
