bdba5c5f78dc434b38e7938b600f95684cc56ea3
Vladyslav Pekker <agilesteel@gmail.com>
2019-09-02 14:38:24 +0200

    Update install-sublime-text.sh

--- install-sublime-text.sh
+++ install-sublime-text.sh
@@ -3,7 +3,7 @@
 # add apt key
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
-# make sure https is enabled
+# make sure https is installed
 sudo apt install -yqqq apt-transport-https
 
 # add repository
